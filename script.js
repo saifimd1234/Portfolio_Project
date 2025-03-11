b3543fb475b1c1521dfd80ddda943c7a7d3b18ec
Mohammad Asad Alam <148976906+asadalam0@users.noreply.github.com>
2025-03-12 03:19:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 document.addEventListener("DOMContentLoaded", function () {
   const projects = document.querySelectorAll(".project");
 
